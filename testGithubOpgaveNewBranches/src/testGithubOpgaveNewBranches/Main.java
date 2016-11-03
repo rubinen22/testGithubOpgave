218d0e5768b3f257ee5cdf28fdd263499dd93b98
rubinen22 <daniel@koru.dk>
2016-11-04 00:43:07 +0100

    Updated Main.

--- testGithubOpgaveNewBranches/src/testGithubOpgaveNewBranches/Main.java
+++ testGithubOpgaveNewBranches/src/testGithubOpgaveNewBranches/Main.java
@@ -3,5 +3,6 @@ package testGithubOpgaveNewBranches;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("012254-7377");
+		System.out.println("212254-7377");
 	}
 }
